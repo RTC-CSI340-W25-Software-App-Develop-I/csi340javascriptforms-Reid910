d3dcf4497ad813eba4ade861470265ba404e68b7
Sam Nelson <samn10.nelson@gmail.com>
2025-01-16 21:03:54 -0800

    finished homework

---
+++ src/index.js
@@ -0,0 +1,48 @@
+
+const section = document.querySelector("section")
+// const fields = { // I tried doing some loops, but I cannot map it, and i cannot foreach so I just hard coded it instead
+//     name,
+//     email,
+//     phone,
+//     reason,
+//     message,
+// }
+function renderSubmit(submission) {
+    const head = document.createElement("h2");
+    head.textContent = "Submitted!";
+    
+    const name = document.createElement("p");
+    name.textContent = submission.name.value;
+    
+    const email = document.createElement("p");
+    email.textContent = submission.email.value;
+    
+    const phone = document.createElement("p");
+    phone.textContent = submission.phone.value;
+    
+    const reason = document.createElement("p");
+    reason.textContent = submission.reason.value;
+    
+    const message = document.createElement("p");
+    message.textContent = submission.message.value;
+    
+    section.append(head,name,email,phone,reason,message); // add elements
+}
+
+const form = document.getElementById("form")
+form.addEventListener("submit", (e) => {
+        e.preventDefault();
+        
+        const submission = {
+            name: e.target.name.value,
+            email: e.target.email.value,
+            phone: e.target.phone.value,
+            reason: e.target.reason.value,
+            message: e.target.message.value,
+        }
+        
+        console.log(submission);
+        section.innerHTML = ""; // clear
+        renderSubmit(submission);
+    }
+)
